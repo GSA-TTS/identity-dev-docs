f2936d24034a44128ea40af93627d3e89f58622a
Zach Margolis <zachmargolis@users.noreply.github.com>
2020-01-29 13:50:29 -0500

    Fix broken link to OIDC on homepage (#98)

--- spec/page.md_spec.rb
+++ spec/page.md_spec.rb
@@ -4,6 +4,10 @@ Dir.glob('_site/**/*.html') do |page|
   RSpec.describe page do
     let(:doc) { Nokogiri::HTML(File.new(page.to_s)) }
 
+    it 'does not have broken Markdown links' do
+      expect(doc.to_s).to_not include(')]')
+    end
+
     it 'links to valid headings' do
       expect(doc).to link_to_valid_headers
     end
