e1813492e08408b10eb0aa66a14a3bd2927c2de0
Nadya Primak <nadya.primak@gsa.gov>
2023-10-27 16:51:03 -0400

    toggling between jwt and pkce moving return to top button

--- assets/js/toggle_view.js
+++ assets/js/toggle_view.js
@@ -1,66 +1,38 @@
-// Elements for Listeners
-const jwtNav = document.getElementById('jwt-nav');
-const pkceNav = document.getElementById('pkce-nav');
-const oidcAuthSuccessButton = document.getElementById('oidc_auth_success_button');
-const oidcAuthErrorButton = document.getElementById('oidc_auth_error_button');
 
-function hideErrorCode(evt) {
-  const errorButton = document.getElementById(`${evt.currentTarget.dataset.selector}_error_button`);
-  errorButton.classList.remove('code-button__selected');
-  const codeSnippet = document.getElementById(`${evt.currentTarget.dataset.selector}_error`);
-  codeSnippet.hidden = true;
-}
 
-function hideSuccessCode(evt) {
-  const successButton = document.getElementById(
-    `${evt.currentTarget.dataset.selector}_success_button`,
-  );
-  successButton.classList.remove('code-button__selected');
-  const codeSnippet = document.getElementById(`${evt.currentTarget.dataset.selector}_success`);
-  codeSnippet.hidden = true;
-}
+// function toggleViewCode(show, hide) {
 
-function showSuccessCode(evt) {
-  evt.currentTarget.classList.add('code-button__selected');
-  const codeSnippet = document.getElementById(`${evt.currentTarget.dataset.selector}_success`);
-  codeSnippet.hidden = false;
-  hideErrorCode(evt);
-}
+// }
 
-function showErrorCode(evt) {
-  evt.currentTarget.classList.add('code-button__selected');
-  const codeSnippet = document.getElementById(`${evt.currentTarget.dataset.selector}_error`);
-  codeSnippet.hidden = false;
-  hideSuccessCode(evt);
-}
+jwt_nav = document.getElementById("jwt-nav")
+pkce_nav = document.getElementById("pkce-nav")
 
-// Unique toggle for OIDC Authorization/Token pages between PKCE and JWT parameters
+jwt_nav.addEventListener("click", togglePKCEView)
+pkce_nav.addEventListener("click", togglePKCEView)
+
 function togglePKCEView(evt) {
-  const pkceElements = document.getElementsByClassName('pkce-only');
-  const jwtDiv = document.getElementById('jwt');
-  const pkceDiv = document.getElementById('pkce');
-  if (evt.currentTarget.id === 'pkce-nav') {
-    for (let i = 0; i < pkceElements.length; i++) {
-      pkceElements[i].hidden = false;
-    }
-    pkceNav.classList.add('code-button__selected');
-    jwtNav.classList.remove('code-button__selected');
-    jwtDiv.hidden = true;
-    pkceDiv.hidden = false;
-  } else {
-    // hide everything in parameters table pertaining to PKCE
-    for (let i = 0; i < pkceElements.length; i++) {
-      pkceElements[i].hidden = true;
-    }
-    pkceNav.classList.remove('code-button__selected');
-    jwtNav.classList.add('code-button__selected');
-    jwtDiv.hidden = false;
-    pkceDiv.hidden = true;
-  }
-}
+    // hide everything with class "pkce-only"
+    elements = document.getElementsByClassName("pkce-only")
+    jwt_nav = document.getElementById("jwt-nav")
+    pkce_nav = document.getElementById("pkce-nav")
+    jwt_div = document.getElementById("jwt")
+    pkce_div = document.getElementById("pkce")
+    if(evt.currentTarget.id == "pkce-nav"){
+        for(i=0; i < elements.length; i++){
+            elements[i].classList.remove("display-none")
+        }
+        pkce_nav.classList.add("code-button__selected")
+        jwt_nav.classList.remove("code-button__selected")
+        jwt_div.classList.add("display-none")
+        pkce_div.classList.remove("display-none")
 
-// Event Listeners
-oidcAuthSuccessButton.addEventListener('click', showSuccessCode);
-oidcAuthErrorButton.addEventListener('click', showErrorCode);
-jwtNav.addEventListener('click', togglePKCEView);
-pkceNav.addEventListener('click', togglePKCEView);
+    } else {
+        for(i=0; i < elements.length; i++){
+            elements[i].classList.add("display-none")
+        }
+        pkce_nav.classList.remove("code-button__selected")
+        jwt_nav.classList.add("code-button__selected")
+        jwt_div.classList.remove("display-none")
+        pkce_div.classList.add("display-none")
+    }
+}
