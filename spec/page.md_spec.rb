944314fff18f3a1a0d684248dbfade53c8061a11
davida marion <davida.marion@gsa.gov>
2023-12-07 10:03:45 -0500

    Add OSC analytics tracking tag

--- spec/page.md_spec.rb
+++ spec/page.md_spec.rb
@@ -1,19 +1,33 @@
 require 'spec_helper'
 
-Dir.glob('_site/**/*.html') do |page|
-  RSpec.describe page do
-    let(:doc) { Nokogiri::HTML(File.new(page.to_s)) }
+def redirect_page?(path)
+  File.read(path).include?('<meta http-equiv="refresh"')
+end
 
-    it 'does not have broken Markdown links' do
-      expect(doc.to_s).to_not include(')]')
-    end
+RSpec.describe 'all pages' do
+  files = Dir.glob('_site/**/*.html')
+  files.reject! {|path| redirect_page?(path) }
+  files.each do |page|
+    describe page do
+      let(:doc) { Nokogiri::HTML(File.new(page.to_s)) }
 
-    it 'links to valid headings' do
-      expect(doc).to link_to_valid_headers
-    end
+      it 'does not have broken Markdown links' do
+        expect(doc.to_s).to_not include(')]')
+      end
 
-    it 'links to valid internal pages' do
-      expect(doc).to link_to_valid_internal_pages
+      it 'links to valid headings' do
+        expect(doc).to link_to_valid_headers
+      end
+
+      it 'links to valid internal pages' do
+        expect(doc).to link_to_valid_internal_pages
+      end
+
+      it 'includes analytics tags' do
+        expect(doc.to_s).to include('https://www.google-analytics.com/analytics.js')
+        expect(doc.to_s).to include('https://dap.digitalgov.gov')
+        expect(doc.to_s).to include('https://www.googletagmanager.com/gtag/js')
+      end
     end
   end
 end
