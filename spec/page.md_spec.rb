75f6ee5d191c1da0f1827b030f920ce2e35025ad
Zach Margolis <zachary.margolis@gsa.gov>
2024-01-25 15:39:00 -0800

    Update description meta tags to remove Markdown
    **Why**: So they can be embedded and summarized more easily

--- spec/page.md_spec.rb
+++ spec/page.md_spec.rb
@@ -28,6 +28,14 @@ RSpec.describe 'all pages' do
         expect(doc.to_s).to include('https://dap.digitalgov.gov')
         expect(doc.to_s).to include('https://www.googletagmanager.com/gtag/js')
       end
+
+      it 'does not include markdown markup in description meta tags', aggregate_failures: true do
+        description = doc.at_css('meta[name=description]')
+        expect(description[:content]).to_not(include('](')) if description
+
+        og_description = doc.at_css('meta[property="og:description"]')
+        expect(og_description[:content]).to_not(include('](')) if og_description
+      end
     end
   end
 end
