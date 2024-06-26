7616de6fb7e2c2dfcd78ba1d659a1c9064298752
AJ Farkas <aj.farkas@gsa.gov>
2024-06-26 15:00:12 -0400

    LG-12800 tabindex on code blocks

--- _plugins/content_code.rb
+++ _plugins/content_code.rb
@@ -1,9 +1,34 @@
 require 'rouge'
+
 # Include tabindex for accessibility reasons
-class HTMLPygmentsA11y < Rouge::Formatters::HTML
+class Rouge::Formatters::HTMLPygmentsA11y < Rouge::Formatters::HTMLPygments
+  def initialize(inner, css_class='codehilite')
+    @inner = inner
+    @css_class = css_class
+  end
+
   def stream(tokens, &b)
     yield %(<div class="highlight"><pre class="#{@css_class}"><code tabindex="0">)
     @inner.stream(tokens, &b)
-    yield "</code></pre></div"
+    yield "</code></pre></div>"
+  end
+end
+
+class Rouge::Formatters::HTMLLegacyA11y < Rouge::Formatters::HTMLLegacy
+  def initialize(opts={})
+    @formatter = opts[:inline_theme] ? HTMLInline.new(opts[:inline_theme])
+               : Rouge::Formatters::HTML.new
+
+
+    @formatter = Rouge::Formatters::HTMLTable.new(@formatter, opts) if opts[:line_numbers]
+
+    if opts.fetch(:wrap, true)
+      @formatter = Rouge::Formatters::HTMLPygmentsA11y.new(@formatter, opts.fetch(:css_class, 'codehilite'))
+    end
+  end
+
+  # @yield the html output.
+  def stream(tokens, &b)
+    @formatter.stream(tokens, &b)
   end
 end
