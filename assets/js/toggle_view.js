5838eaf22c433837c2d1c47106fb5cd8948feecb
Nadya Primak <nadya.primak@gsa.gov>
2024-11-08 17:25:37 -0500

    make pkce separate page so anchor links work

--- assets/js/toggle_view.js
+++ assets/js/toggle_view.js
@@ -1,6 +1,4 @@
 // Elements for Listeners
-const jwtNav = document.getElementById('jwt-nav');
-const pkceNav = document.getElementById('pkce-nav');
 const oidcAuthTab1Button = document.getElementById('oidc_auth_tab1_button');
 const oidcAuthTab2Button = document.getElementById('oidc_auth_tab2_button');
 const oidcTokenTab1Button = document.getElementById('oidc_token_tab1_button');
@@ -17,6 +15,7 @@ const samlLogoutTab1Button = document.getElementById('saml_logout_tab1_button');
 const samlLogoutTab2Button = document.getElementById('saml_logout_tab2_button');
 const samlLogoutResponseTab1Button = document.getElementById('saml_logout_response_tab1_button');
 const samlLogoutResponseTab2Button = document.getElementById('saml_logout_response_tab2_button');
+
 
 function hideTabTwoCode(evt) {
   const errorButton = document.getElementById(`${evt.currentTarget.dataset.selector}_tab2_button`);
@@ -48,50 +47,15 @@ function showTabTwoCode(evt) {
   hideTabOneCode(evt);
 }
 
-// Unique toggle for OIDC Authorization/Token pages between PKCE and JWT parameters
-function togglePKCEView(evt) {
-  const pkceElements = document.getElementsByClassName('pkce-only');
-  const jwtElements = document.getElementsByClassName('jwt-only');
-  const jwtDiv = document.getElementById('jwt');
-  const pkceDiv = document.getElementById('pkce');
-  if (evt.currentTarget.id === 'pkce-nav') {
-    for (let i = 0; i < pkceElements.length; i++) {
-      pkceElements[i].hidden = false;
-    }
-    for (let i = 0; i < jwtElements.length; i++) {
-      jwtElements[i].hidden = true;
-    }
-    pkceNav.classList.add('code-button__selected');
-    jwtNav.classList.remove('code-button__selected');
-    jwtDiv.hidden = true;
-    pkceDiv.hidden = false;
-  } else {
-    // hide everything in parameters table pertaining to PKCE
-    for (let i = 0; i < pkceElements.length; i++) {
-      pkceElements[i].hidden = true;
-    }
-    for (let i = 0; i < jwtElements.length; i++) {
-      jwtElements[i].hidden = false;
-    }
-    pkceNav.classList.remove('code-button__selected');
-    jwtNav.classList.add('code-button__selected');
-    jwtDiv.hidden = false;
-    pkceDiv.hidden = true;
-  }
-}
 
 // Event Listeners
 if (oidcAuthTab1Button) {
   oidcAuthTab1Button.addEventListener('click', showTabOneCode);
   oidcAuthTab2Button.addEventListener('click', showTabTwoCode);
-  jwtNav.addEventListener('click', togglePKCEView);
-  pkceNav.addEventListener('click', togglePKCEView);
 }
 if (oidcTokenTab1Button) {
   oidcTokenTab1Button.addEventListener('click', showTabOneCode);
   oidcTokenTab2Button.addEventListener('click', showTabTwoCode);
-  jwtNav.addEventListener('click', togglePKCEView);
-  pkceNav.addEventListener('click', togglePKCEView);
 }
 if (oidcUserInfoTab1Button) {
   oidcUserInfoTab1Button.addEventListener('click', showTabOneCode);
