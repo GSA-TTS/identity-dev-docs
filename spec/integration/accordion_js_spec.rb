993473c94871640d2318049ce7224198212c2782
Howard Miller <3620291+h-m-m@users.noreply.github.com>
2025-06-05 17:27:19 -0400

    Allow auto-expand of accordions based on URL

--- spec/integration/accordion_js_spec.rb
+++ spec/integration/accordion_js_spec.rb
@@ -2,9 +2,25 @@ require 'capybara_helper'
 require 'pry-byebug'
 
 RSpec.describe 'accordions on /oidc/authorization/', :js, type: :feature do
-  it 'loads the page' do
-    visit '/oidc/authorization/'
-    first_summary = find_all('summary').first
-    expect(first_summary.text).to eq 'Type of Service Level'
+  it 'is not expanded by default' do
+    visit '/oidc/authorization'
+    expect(page).to have_element('dt')
+    expect(page).to have_no_element('dd')
+    accordion_button = find('#service_level button')
+    expect(accordion_button.native.attribute('aria-expanded')).to eq('false')
+  end
+
+  it 'is expanded with an anchor in the URL' do
+    visit '/oidc/authorization#service_level'
+    expect(page).to have_element('dt')
+    expect(page).to have_element('dd')
+    definition = find('#service_level ~ dd').text
+    expect(definition).to include('acr.login.gov:verified')
+    accordion_button = find('#service_level > button')
+    expect(accordion_button.native.attribute('aria-expanded')).to eq('true')
+
+    visit '/oidc/authorization#aal_values'
+    next_definition = find('#aal_values ~ dd').text
+    expect(next_definition).to include('http://idmanagement.gov/ns/assurance/aal/2')
   end
 end
