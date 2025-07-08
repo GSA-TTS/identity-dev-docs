86d2d475dc37d2aa3e20407289a01c47a370511d
Michael Ambroselli <michael.ambroselli@gsa.gov>
2025-07-08 16:01:01 -0400

    add links to accordions

--- assets/js/anchor_accordion.js
+++ assets/js/anchor_accordion.js
@@ -1,16 +1,22 @@
 (() => {
-  const onChange = (_event) => {
+  const jumpToAccordion = (_event) => {
     const urlHash = new URL(document.URL).hash;
-    const idFromHash = urlHash.match(/^(#[A-Za-z0-9_-]+)$/);
+    const idFromHash = urlHash.match(/^#([A-Za-z0-9_-]+)$/);
     if (idFromHash) {
-      const accordionHeading = document.querySelector(
-        `${idFromHash[1]}.usa-accordion__heading > button`,
-      );
+      const accordionHeading = document.getElementById(idFromHash[1]).previousElementSibling.querySelector('.usa-accordion__heading > button');
       if (accordionHeading) {
         accordionHeading.click();
       }
     }
   };
-  document.addEventListener('DOMContentLoaded', onChange);
-  window.addEventListener('hashchange', onChange);
+
+  const addAccordionLinks = (_event) => {
+    const accordions = Array.from(document.querySelectorAll('.usa-accordion__content'));
+    const accordion_id_list = accordions.slice(1).reduce((a, v) => a?`${a}, #${v.id}`:`#${v.id}`, null);
+    anchors.add(accordion_id_list);
+  };
+
+  document.addEventListener('DOMContentLoaded', jumpToAccordion);
+  document.addEventListener('DOMContentLoaded', addAccordionLinks);
+  window.addEventListener('hashchange', jumpToAccordion);
 })();
