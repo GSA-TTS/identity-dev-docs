0b08d894845d57879bc76c0733b0a0986ecef424
AJ Farkas <aj.farkas@gsa.gov>
2025-08-19 12:17:51 -0400

    fix lint errors

--- spec/e2e/accordion.test.js
+++ spec/e2e/accordion.test.js
@@ -1,4 +1,4 @@
-import { describe, before, after, test, it } from 'node:test';
+import { describe, before, after, test } from 'node:test';
 import assert from 'node:assert';
 import puppeteer from 'puppeteer';
 import { createServer } from 'http';
@@ -6,14 +6,15 @@ import handler from 'serve-handler';
 import getPort from 'get-port';
 
 const isEqual = assert.deepStrictEqual;
-const notEqual = assert.notDeepStrictEqual;
 
 describe('accordions', () => {
-  let server, port, browser, page;
+  let server;
+  let port;
+  let browser;
+  let page;
 
   before(async () => {
     port = await getPort();
-    console.log(`http://localhost:${port}`);
     browser = await puppeteer.launch();
     server = createServer((req, res) => {
       handler(req, res, { public: '_site' });
@@ -29,23 +30,21 @@ describe('accordions', () => {
 
     await test('is not expanded by default', async () => {
       await page.goto(`http://localhost:${port}/`);
-      const accordionBtn = await page
-        .waitForSelector('#integration-checklist-accordion > button');
-      const accordionContent = await page
-        .waitForSelector('#home-register-checklist');
-      
+      const accordionBtn = await page.waitForSelector('#integration-checklist-accordion > button');
+      const accordionContent = await page.waitForSelector('#home-register-checklist');
+
       isEqual(
-        await accordionBtn.evaluate(el => el.innerText),
+        await accordionBtn.evaluate((el) => el.innerText),
         'Information you need to register your application',
         'Accordion button is missing correct text',
       );
       isEqual(
-        await accordionContent.evaluate(el => el.checkVisibility()),
+        await accordionContent.evaluate((el) => el.checkVisibility()),
         false,
         'Accordion should not be expanded',
       );
       isEqual(
-        await accordionBtn.evaluate(el => el.getAttribute('aria-expanded')),
+        await accordionBtn.evaluate((el) => el.getAttribute('aria-expanded')),
         'false',
         'aria-expanded incorrectly set to "true"',
       );
@@ -53,26 +52,23 @@ describe('accordions', () => {
 
     await test('is expanded with an anchor in the URL', async () => {
       await page.goto(`http://localhost:${port}/#integration-checklist-accordion`);
-      const accordionBtn = await page
-        .waitForSelector('#integration-checklist-accordion > button');
-      const accordionContent = await page
-        .waitForSelector('#home-register-checklist');
+      const accordionBtn = await page.waitForSelector('#integration-checklist-accordion > button');
+      const accordionContent = await page.waitForSelector('#home-register-checklist');
 
       isEqual(
-        await accordionContent.evaluate(el => el.checkVisibility()),
+        await accordionContent.evaluate((el) => el.checkVisibility()),
         true,
         'Accordion should be expanded',
       );
 
-      const definition = await page
-        .waitForSelector('#integration-checklist-accordion ~ dd');
+      const definition = await page.waitForSelector('#integration-checklist-accordion ~ dd');
       assert.match(
-        await definition.evaluate(el => el.innerText),
-        new RegExp('Inter-agency agreement application name'),
+        await definition.evaluate((el) => el.innerText),
+        /Inter-agency agreement application name/,
         'Checklist content is incorrect',
       );
       isEqual(
-        await accordionBtn.evaluate(el => el.getAttribute('aria-expanded')),
+        await accordionBtn.evaluate((el) => el.getAttribute('aria-expanded')),
         'true',
         'aria-expanded incorrectly set to "false"',
       );
@@ -84,13 +80,12 @@ describe('accordions', () => {
 
     await test('is not expanded by default', async () => {
       await page.goto(`http://localhost:${port}/oidc/authorization`);
-      const accordionBtn = await page
-        .waitForSelector('#service_level > button');
+      const accordionBtn = await page.waitForSelector('#service_level > button');
 
       assert(await page.waitForSelector('dt'));
       assert(await page.waitForSelector('dd'));
       isEqual(
-        await accordionBtn.evaluate(el => el.getAttribute('aria-expanded')),
+        await accordionBtn.evaluate((el) => el.getAttribute('aria-expanded')),
         'false',
         'aria-expanded incorrectly set to "true"',
       );
@@ -98,27 +93,21 @@ describe('accordions', () => {
 
     await test('is expanded with an anchor in the URL', async () => {
       await page.goto(`http://localhost:${port}/oidc/authorization#service_level`);
-      const accordionBtn = await page
-        .waitForSelector('#service_level > button');
-      const levelDefinition = await page
-        .waitForSelector('#service_level ~ dd');
+      const accordionBtn = await page.waitForSelector('#service_level > button');
+      const levelDefinition = await page.waitForSelector('#service_level ~ dd');
 
       assert.match(
-        await levelDefinition.evaluate(el => el.innerText),
-        new RegExp('acr.login.gov:verified'),
+        await levelDefinition.evaluate((el) => el.innerText),
+        /acr\.login\.gov:verified/,
       );
-      isEqual(
-        await accordionBtn.evaluate(el => el.getAttribute('aria-expanded')),
-        'true',
-      );
+      isEqual(await accordionBtn.evaluate((el) => el.getAttribute('aria-expanded')), 'true');
 
       await page.goto(`http://localhost:${port}/oidc/authorization#aal_values`);
-      const aalDefinition = await page
-        .waitForSelector('#aal_values ~ dd');
+      const aalDefinition = await page.waitForSelector('#aal_values ~ dd');
 
       assert.match(
-        await aalDefinition.evaluate(el => el.innerText),
-        new RegExp('http://idmanagement.gov/ns/assurance/aal/2'),
+        await aalDefinition.evaluate((el) => el.innerText),
+        /http:\/\/idmanagement.gov\/ns\/assurance\/aal\/2/,
       );
     });
   });
